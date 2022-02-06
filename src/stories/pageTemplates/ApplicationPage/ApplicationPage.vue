85304291fd70493bf159ae2ddaf8e77641c6bfb3
Devon Chau <devonpis@gmail.com>
2022-02-07 08:09:23 +1000

    add lint-storybook

--- src/stories/pageTemplates/ApplicationPage/ApplicationPage.vue
+++ src/stories/pageTemplates/ApplicationPage/ApplicationPage.vue
@@ -4,7 +4,7 @@ import Template from '../../../../src/template-pages/application-page.html';
 import Header from '../../components/layout/header/Header/Header.vue';
 import Access from '../../components/layout/header/Access/Access.vue';
 import BreadcrumbGlobal from '../../components/layout/Breadcrumb/BreadcrumbGlobal.vue';
-import Options from '../../components/layout/content/Options/Options.vue';
+import Options from '../../components/layout/content/Options/Options.vue'
 
 export default {
   components: {
